defaed66cab29b90a5e9d8a55e04079387acf06e
davidehu <53004896+davidehu@users.noreply.github.com>
2023-11-01 00:54:08 +0000

    Add files via upload

---
+++ prog_assign2022/PCR_primers.py
@@ -0,0 +1,23 @@
+#Template for program to ouput primer sequences for PCR reaction
+
+#Write first any import statements that you need in your program
+
+
+
+
+#Define here any functions you will be using
+
+
+
+
+#The following if statement does not affect the working of the program,
+#but will allow to test your functions even if the rest of the program
+#does not work
+
+if __name__=="__main__":
+
+    #Remove the pass statement below and write the rest of the
+    #program inside this if block
+    #Do not forget indentation
+
+    pass
